c582d193da49494a6d37e326d6801f702b6461b4
adamb924 <adamb924@gmail.com>
2022-09-30 14:03:54 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,74 @@
+function displayFeedbackScreen() {
+    let screen = document.getElementById("dark-screen");
+    let container = document.getElementById("feedback-container");
+
+    let oldDisplayValue = screen.style.display;
+    if( oldDisplayValue != "block" ) {
+        screen.style.display = "block";
+        container.style.display = "block";
+    } else {
+        screen.style.display = "none";
+        container.style.display = "none";
+    }
+}
+
+
+function sendFeedback() {
+    const messageContent =  document.getElementById("messageContent").value;
+    const timeStamp = new Date().getTime();
+
+    var db = firebase.firestore();
+    db.collection("messages").add({
+        content: messageContent,
+        time: timeStamp
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+        hideFeedbackContainer();
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+        hideFeedbackContainer();
+    });
+}
+
+function hideFeedbackContainer() {
+    document.getElementById("dark-screen").style.display = "none";
+    document.getElementById("feedback-container").style.display = "none";
+}
+
+function stopEvent() {
+    event.stopPropagation();
+}
+
+function scrollToChapter(selectObject) {
+    if( selectObject.value == "top" ) {
+        window.scroll(0,0);
+        hideNavbar();
+    } else {
+        document.getElementById(selectObject.value).scrollIntoView();
+    }
+}
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos && currentScrollPos != 0) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    hideNavbar();
+  }
+  prevScrollpos = currentScrollPos;
+  document.getElementById("list").value = "";
+} 
+
+function hideNavbar() {
+    if( document.getElementById("navbar") ) {
+        document.getElementById("navbar").style.top = "-" + document.getElementById("navbar").offsetHeight + "px";
+    }
+}
+
+window.onload = function(e){ 
+    hideNavbar();
+}
